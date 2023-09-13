0fe776298b90c5328076bf9bd6ba26f4de5c8acc
mohit49 <mohitanim@gmail.com>
2023-09-14 02:06:40 +0530

    second changes

--- js/index.js
+++ js/index.js
@@ -14,6 +14,39 @@ const homePageInit = () => {
             });
     }
 
+    window.addEventListener('beforeinstallprompt', function(event) {
+        // Prevent the default browser install prompt
+        event.preventDefault();
+
+        // Store the event to use it later
+        const deferredPrompt = event;
+
+        // Show your custom install button or notification
+        const installButton = document.getElementById('install-button');
+        installButton.style.display = 'block';
+
+        // Handle the user's click on the custom install button
+        installButton.addEventListener('click', function() {
+            // Show the browser's install prompt
+            deferredPrompt.prompt();
+
+            // Wait for the user to respond to the install prompt
+            deferredPrompt.userChoice.then(function(choiceResult) {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the install prompt');
+                } else {
+                    console.log('User dismissed the install prompt');
+                }
+
+                // Reset the deferredPrompt for future use
+                deferredPrompt = null;
+
+                // Hide the custom install button
+                installButton.style.display = 'none';
+            });
+        });
+    });
+
     $mainButton.addEventListener('click', function(){
         alert('Hello, World')
     })
