c32f8f97e46a82223fc307f97e6ea594ff01fad7
mohit49 <mohitanim@gmail.com>
2023-09-14 01:56:28 +0530

    second changes

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const selectors = {
 const homePageInit = () => {
     // register service worker
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('../service-worker.js')
+        navigator.serviceWorker.register('./service-worker.js')
             .then(function(registration) {
                 console.log('Service Worker registered with scope:', registration.scope);
             })
