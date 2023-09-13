72b164a595de7b46fb7f5783173f5855a3fef59d
mohit49 <mohitanim@gmail.com>
2023-09-14 01:35:02 +0530

    second changes

--- js/index.js
+++ js/index.js
@@ -3,6 +3,17 @@ const selectors = {
   mainBannerButton: ".main-banner-button",
 };
 const homePageInit = () => {
+    // register service worker
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('service-worker.js')
+            .then(function(registration) {
+                console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch(function(error) {
+                console.error('Service Worker registration failed:', error);
+            });
+    }
+
     $mainButton.addEventListener('click', function(){
         alert('Hello, World')
     })
---
+++ js/service-worker.js
@@ -0,0 +1,21 @@
+// Service Worker implementation
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('simple-pwa-cache').then(function(cache) {
+            return cache.addAll([
+                './',
+                'index.html',
+                'icon.png', // Update with your icon file
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
